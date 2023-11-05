164c94e8364c0ae27bc4caf74a08cd5fe94d4be9
Jing-Ting Dong <stevendong2002@gmail.com>
2023-11-05 16:59:10 -0500

    Resize TV Channels

--- src/tv-channel.js
+++ src/tv-channel.js
@@ -23,18 +23,31 @@ export class TvChannel extends LitElement {
   static get styles() {
     return css`
      :host {
-            display: flex;      /* Changed from inline-flex to flex */
-            flex-direction: column; /* Stack children vertically */
-            align-items: flex-start; /* Align items to the start of the flex container */
-}
-
-.wrapper {
-            padding: 16px;
-            background-color: #eeeeee;
+            display: block; /* Changed to block to contain the flex item */
+            width: 210px; /* Set the width of the host */
+            height: 48px; /* Set the height of the host */
+            box-sizing: border-box; /* Include padding and border in the element's total width and height */
             margin-bottom: 16px; /* Add space between the items */
-            width: 100%; /* Full width to the container for consistency */
-}
+        }
 
+        .wrapper {
+            display: flex; /* Use flexbox to align items */
+            align-items: center; /* Center items vertically */
+            justify-content: center; /* Center items horizontally */
+            padding: 4px; /* Adjust padding to fit the content within 210x48 size */
+            background-color: #eeeeee;
+            width: 100%; /* Use the full width provided by the host */
+            height: 100%; /* Use the full height provided by the host */
+            box-sizing: border-box; /* Include padding and border in the element's total width and height */
+        }
+
+        h3, h4 {
+            margin: 0; /* Remove default margins */
+            white-space: nowrap; /* Prevent wrapping */
+            overflow: hidden; /* Hide overflow */
+            text-overflow: ellipsis; /* Add ellipsis for overflow text */
+            font-size: smaller; /* Adjust font size to fit the container */
+        }
     `;
   }
   // LitElement rendering template of your element
